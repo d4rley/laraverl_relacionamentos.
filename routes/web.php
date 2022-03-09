d28216cf3f1b282c8c3405b73a660b6c24870832
freptchunai <felipedarley72@gmail.com>
2022-03-09 11:28:50 -0300

    organizando e comentando o arquivo de rotas

--- routes/web.php
+++ routes/web.php
@@ -6,29 +6,23 @@ use App\Http\Controllers\OneToManyController;
 use App\Http\Controllers\ManyToManyController;
 use App\Http\Controllers\PolymorphicsController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
+// Rotas Ultilizando one to one
 Route::get('/one-to-one', [OneToOneController::class,'oneToOne'])->name('one_to_one');
 Route::get('/one-to-one-inverse', [OneToOneController::class,'oneToOneinverse'])->name('one_to_one_inverse');
 Route::get('/one-to-one-insert', [OneToOneController::class,'oneToOneinsert'])->name('one_to_one-inserte');
-
+//Rotas ultilizando one to many ou inversa causa
 Route::get('/one-to-many', [OneToManyController::class, 'oneToMany'])->name('one-to-many');
 Route::get('/many-to-one', [OneToManyController::class, 'ManyToOne'])->name('many-to-one');
 Route::get('/one-to-many-two', [OneToManyController::class, 'oneToManyTwo'])->name('one-to-many-two');
 Route::get('/one-to-many-insert', [OneToManyController::class, 'oneToManyInsert'])->name('one-to-many-insert');
+//Rota ultilizando has many
 Route::get('/has-many-through', [OneToManyController::class, 'hasManyThrough'])->name('has-many-through');
+//Rotas ultilizando many to many
 Route::get('/many-to-many', [ManyToManyController::class,'ManyToMany']);
 Route::get('/many-to-many-inverse', [ManyToManyController::class, 'ManyToManyInverse']);
 Route::get('/many-to-many-insert', [ManyToManyController::class, 'ManyToManyInsert']);
+//Rotas ultilizndo Polimorfismo 
 Route::get('polymorphics',[PolymorphicsController::class, 'polymorphic']);
 Route::get('polymorphics-insert',[PolymorphicsController::class, 'polymorphicInsert']);
 
